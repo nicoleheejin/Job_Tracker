025ba7f04c3f3081ca932be63de785b6d05e4b02
Nicole Landskroener <landskroenernh@gmail.com>
2017-09-20 11:42:48 -0700

    Initial scaffolding for job tracker

---
+++ job-tracker.rb
@@ -0,0 +1,20 @@
+puts "Welcome to your job tracker, you smart individual."
+
+while true do
+  puts "What would you like to do?"
+  input = gets.chomp
+  
+  if input == "list"
+  
+  else if input == "find"
+    
+  else if input == "insert" 
+
+  else if input == "exit"
+    break
+  else
+    puts "Please input a valid command."
+  end
+end
+
+
